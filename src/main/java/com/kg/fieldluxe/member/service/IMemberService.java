bca0b28e4d21d5508bb9e24681c5bd00f25932b9
JuhyubLee <60839817+JuhyubLee@users.noreply.github.com>
2021-01-25 10:40:28 +0900

    member, authority
    유저, 권한 관련

--- src/main/java/com/kg/fieldluxe/member/service/IMemberService.java
+++ src/main/java/com/kg/fieldluxe/member/service/IMemberService.java
@@ -1,5 +1,18 @@
 package com.kg.fieldluxe.member.service;
 
+import java.util.List;
+
+import com.kg.fieldluxe.member.model.MemberVO;
+
 public interface IMemberService {
 
+	void insertMember(MemberVO mem);
+	void updateBan(MemberVO mem);
+	void updateMember(MemberVO mem);
+	void deleteMember(String email);
+	void deleteAuth(String email);
+	String getPassword(String email);
+	MemberVO getMember(String email);
+	List<MemberVO> getMemberList(int page, String word);
+	Integer getMemberCount(String word);
 }
